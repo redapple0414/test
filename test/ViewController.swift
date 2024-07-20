81e3b11225b47f09ca6e1638d9c513c49d09f3e9
redapple0414 <redapple20100414@gmail.com>
2024-07-20 17:40:35 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by akari on 2024/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
